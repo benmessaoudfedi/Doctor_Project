beff2ceca51d83e5ff27b43302a71779b6cf3944
unknown <benmessaoudfedi@gmail.com>
2021-05-14 21:29:12 +0100

    sec commit

--- Doctor/models.py
+++ Doctor/models.py
@@ -12,7 +12,7 @@ departments=[('Cardiologist','Cardiologist'),
 
 class Doctor(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic= models.ImageField(upload_to='static/assets/DoctorProfilePic/', null=True, blank=True)
+    profile_pic= models.ImageField(upload_to='static/assets/DoctorProfilePic/',default='static/assets/DoctorProfilePic/photo_default.jpg', null=True, blank=True)
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20,null=True)
     department= models.CharField(max_length=50,choices=departments,default='Cardiologist')
